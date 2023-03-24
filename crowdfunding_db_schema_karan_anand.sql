77a1f43bb2da25b3d5fbeef40461430a0efbc146
Karan Anand <karananand87@gmail.com>
2023-03-23 20:11:46 -0400

    first commit

--- crowdfunding_db_schema_karan_anand.sql
+++ crowdfunding_db_schema_karan_anand.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "campaign" (
+CREATE TABLE campaign (
     "cf_id" int   NOT NULL,
     "contact_id" int   NOT NULL,
     "company_name" varchar(100)   NOT NULL,
@@ -18,7 +18,7 @@ CREATE TABLE "campaign" (
      )
 );
 
-CREATE TABLE "category" (
+CREATE TABLE category (
     "category_id" varchar(10)   NOT NULL,
     "category_name" varchar(50)   NOT NULL,
     CONSTRAINT "pk_category" PRIMARY KEY (
@@ -26,7 +26,7 @@ CREATE TABLE "category" (
      )
 );
 
-CREATE TABLE "subcategory" (
+CREATE TABLE subcategory (
     "subcategory_id" varchar(10)   NOT NULL,
     "subcategory_name" varchar(50)   NOT NULL,
     CONSTRAINT "pk_subcategory" PRIMARY KEY (
@@ -34,7 +34,7 @@ CREATE TABLE "subcategory" (
      )
 );
 
-CREATE TABLE "contacts" (
+CREATE TABLE contacts (
     "contact_id" int   NOT NULL,
     "first_name" varchar(50)   NOT NULL,
     "last_name" varchar(50)   NOT NULL,
@@ -44,12 +44,19 @@ CREATE TABLE "contacts" (
      )
 );
 
-
-ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+ALTER TABLE campaign ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
 REFERENCES "contacts" ("contact_id");
 
-ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+ALTER TABLE campaign ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
 REFERENCES "category" ("category_id");
 
-ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+ALTER TABLE campaign ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
 REFERENCES "subcategory" ("subcategory_id");
+
+select * from campaign;
+
+select * from category;
+
+select * from contacts;
+
+select * from subcategory;
